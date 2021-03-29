2747fa90e2f4ddcb3eeee575f76a0db364e4b8d2
Raymond Lehlohonolo Mashigo <thlogi712@gmail.com>
2021-03-29 21:07:48 +0200

    addind my budget app to repo

---
+++ src/components/RemainingBudget.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RemainingBudget = () => {
+	return (
+		<div className='alert alert-success'>
+			<span>Remaining: R1000</span>
+		</div>
+	);
+};
+
+export default RemainingBudget;
